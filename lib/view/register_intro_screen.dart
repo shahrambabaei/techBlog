40c80a4fc45537ce8f889992c860c1d7d81874fd
shahram babaei <shahram.b6996@gmail.com>
2025-01-15 23:34:54 +0330

    feat:add libarary validators for regExp

--- lib/view/register_intro_screen.dart
+++ lib/view/register_intro_screen.dart
@@ -1,6 +1,8 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:techblog/constant/app_color.dart';
+import 'package:validators/validators.dart';
 import 'package:techblog/constant/my_string.dart';
 import 'package:techblog/gen/assets.gen.dart';
 
@@ -33,55 +35,7 @@ class RegisterIntroScreen extends StatelessWidget {
                 padding: const EdgeInsets.only(top: 50),
                 child: ElevatedButton(
                     onPressed: () {
-                      showModalBottomSheet(
-                          isScrollControlled: true,
-                          context: context,
-                          builder: (context) {
-                            return Padding(
-                              padding: EdgeInsets.only(
-                                  bottom:
-                                      MediaQuery.of(context).viewInsets.bottom),
-                              child: Container(
-                                height: size.height / 2,
-                                width: size.width,
-                                decoration: const BoxDecoration(
-                                    borderRadius: BorderRadius.only(
-                                        topRight: Radius.circular(24),
-                                        topLeft: Radius.circular(24)),
-                                    color: Colors.white),
-                                child: Column(
-                                  children: [
-                                    const SizedBox(
-                                      height: 100,
-                                    ),
-                                    Text(
-                                      MyStrings.insertYourEmail,
-                                      style: textTheme.headlineMedium!.copyWith(
-                                          fontSize: 16,
-                                          fontWeight: FontWeight.bold),
-                                    ),
-                                    Padding(
-                                      padding: const EdgeInsets.symmetric(
-                                          horizontal: 30, vertical: 20),
-                                      child: TextField(
-                                        textAlign: TextAlign.center,
-                                        decoration: InputDecoration(
-                                          hintText: "emxample@gmail.com",
-                                          hintStyle: textTheme.headlineSmall,
-                                        ),
-                                      ),
-                                    ),
-                                    const SizedBox(
-                                      height: 50,
-                                    ),
-                                    ElevatedButton(
-                                        onPressed: () {},
-                                        child: const Text("ادامه"))
-                                  ],
-                                ),
-                              ),
-                            );
-                          });
+                      _showEmailbottomSheet(context, size, textTheme);
                     },
                     child: Text(
                       "بزن بریم",
@@ -93,5 +47,58 @@ class RegisterIntroScreen extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  Future<dynamic> _showEmailbottomSheet(
+      BuildContext context, Size size, TextTheme textTheme) {
+    return showModalBottomSheet(
+        isScrollControlled: true,
+        context: context,
+        builder: (context) {
+          return Padding(
+            padding: EdgeInsets.only(
+                bottom: MediaQuery.of(context).viewInsets.bottom),
+            child: Container(
+              height: size.height / 2,
+              width: size.width,
+              decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                      topRight: Radius.circular(24),
+                      topLeft: Radius.circular(24)),
+                  color: Colors.white),
+              child: Column(
+                children: [
+                  const SizedBox(
+                    height: 100,
+                  ),
+                  Text(
+                    MyStrings.insertYourEmail,
+                    style: textTheme.headlineMedium!
+                        .copyWith(fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 30, vertical: 20),
+                    child: TextField(
+                      style: textTheme.headlineMedium,
+                      textAlign: TextAlign.center,
+                      decoration: InputDecoration(
+                        hintText: "emxample@gmail.com",
+                        hintStyle: textTheme.headlineSmall,
+                      ),
+                      onChanged: (value) {
+                        log("$value   is Email: ${isEmail(value)}");
+                      },
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 50,
+                  ),
+                  ElevatedButton(onPressed: () {}, child: const Text("ادامه"))
+                ],
+              ),
+            ),
+          );
+        });
   }
 }
